afd7a5c37354d9f6c00951ab124b69f6aaeba65b
ansteh <stehle.andre@googlemail.com>
2017-08-06 23:57:54 +0200

    explore xml for properties

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const ParseXbrl = require('parse-xbrl');
 const got       = require('got');
 const fs        = require('fs');
+const xmlParser = require('xml2json');
+const _         = require('lodash');
 
 const downloadFile = (sourceUrl, filename) => {
   got.stream(sourceUrl).pipe(fs.createWriteStream(`${__dirname}/resources/${filename}`));
@@ -19,4 +21,35 @@ const parse = (filename) => {
 // parse('amzn-10-Q.xml');
 
 // downloadFile('https://www.sec.gov/Archives/edgar/data/1467858/000146785817000028/gm-20161231.xml', 'gm-10-Q-2016.xml');
-parse('gm-10-Q-2016.xml');
+// parse('gm-10-Q-2016.xml');
+
+const parseFile = (filename) => {
+  const filePath = `${__dirname}/resources/${filename}`;
+
+  return fs.readFileAsync(filePath, 'utf8')
+    .then(xmlStr => xmlParser.toJson(xmlStr));
+}
+
+const filter = (stack, needle) => {
+  needle = _.toLower(needle);
+  return _.filter(stack, (str) => {
+    return _.includes(_.toLower(str), needle);
+  });
+};
+
+parseFile('gm-10-Q-2016.xml')
+  .then((data) => {
+    // console.log(JSON.stringify(data, null, 2));
+    let jsonObj = JSON.parse(data);
+    let documentJson = jsonObj[Object.keys(jsonObj)[0]];
+
+    let keys = Object.keys(documentJson);
+    // console.log(keys.length);
+    // console.log(JSON.stringify(keys, null, 2));
+
+    console.log(documentJson['us-gaap:CommonStockDividendsPerShareDeclared']);
+
+    // let results = filter(keys, 'Dividend');
+    // console.log(results);
+  })
+  .catch(console.log);
