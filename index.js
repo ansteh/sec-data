88ab1017cca1f47fafc182839fef412d54f837ad
ansteh <stehle.andre@googlemail.com>
2017-08-12 23:56:34 +0200

    refactor table crawler

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const Filings = require('./lib/filings');
 const testParseFilingsDocument = () => {
   let document = fs.readFileSync(`${__dirname}/resources/example.html`);
   let filings = Filings.Crawler.parseDocument(document);
-  console.log(filings);
+  console.log(JSON.stringify(filings, null, 2));
 
   console.log(Filings.Queries.getCompany({ cik: '0001467858' }));
 }
