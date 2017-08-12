3365c73960afadc520da0d0abc2b402019a6adb3
ansteh <stehle.andre@googlemail.com>
2017-08-12 15:14:19 +0200

    parse filing for xml files

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ const testDownloadFilingFile = () => {
 
 const testCrawlFilingDocument = () => {
   let sourceUrl = 'https://www.sec.gov/Archives/edgar/data/1467858/000146785817000028/0001467858-17-000028-index.htm';
-  let filing = Filing.Crawler.parseDocument('gm-test-filing.html');
+  let document = fs.readFileSync(`${__dirname}/resources/gm-test-filing.html`);
+  let filing = Filing.Crawler.parseDocument(document);
   console.log(filing);
 };
 
