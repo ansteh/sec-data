50f2cd41f436662c392062e07122dc5ae46a1d55
ansteh <stehle.andre@googlemail.com>
2017-08-13 23:40:37 +0200

    download instance files

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const fs        = require('fs');
 
-// const saveEaxmple = () => {
-//   let sourceUrl = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001467858&type=10-K&dateb=&owner=include&count=40';
-//   got.stream(sourceUrl).pipe(fs.createWriteStream(`${__dirname}/resources/example.html`));
-// };
-
 const Stock = require('./lib/stock');
 
 const GM = {
   ticker: 'GM',
   name: 'General Motors Co',
   cik: '0001467858',
-  search: {}
+  filings: {}
 };
 
 // Stock.remove(GM)
 //   .then(() => Stock.create(GM));
 
-Stock.crawlAnnualFilings('GM')
-  .then(console.log)
-  .catch(console.log);
+// Stock.crawlAnnualFilings('GM')
+//   .then(console.log)
+//   .catch(console.log);
+
+// Stock.downloadAnnualFiles('GM')
+//   .then(console.log)
+//   .catch(console.log);
