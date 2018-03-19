beac4eabf6d461118f452f852d2da384e904f846
ansteh <stehle.andre@googlemail.com>
2018-03-19 21:13:58 +0100

    deploy prototype

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', express.static(path.join(__dirname, '/dist')));
+app.use('/', express.static(path.join(__dirname, '../dist')));
 
 app.use(require('./api/stocks'));
 app.use(require('./api/stock'));
