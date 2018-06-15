a150f10e09f59e46831c885b99a9ebb3e636f471
ansteh <stehle.andre@googlemail.com>
2018-06-15 23:41:40 +0200

    account portfolio

--- server/api/stocks/controller.js
+++ server/api/stocks/controller.js
@@ -2,19 +2,6 @@ const _       = require('lodash');
 const Stocks  = require('./model.js');
 const Filters = require('./filter.js');
 
-const prepare = (results) => {
-  return _
-    .chain(results)
-    .map((row) => {
-      return {
-        ticker: row.ticker,
-        params: _.pick(row, ['margin', 'PE', 'PB'])
-      }
-    })
-    .keyBy('ticker')
-    .value();
-};
-
 const filter = (options) => {
   // const DerivedTrailingTwelveMonthsEarningsPerShareDiluted
   const paths = {
@@ -37,6 +24,19 @@ const filter = (options) => {
 
   const { pipeline } = testAggregate;
 
+  appendValuationsTo(paths, pipeline);
+
+  pipeline.push({
+    $match: {
+      margin: { $ne : null }
+    }
+  });
+
+  return Stocks.aggregate(testAggregate)
+    .then(prepare)
+};
+
+const appendValuationsTo = (paths, pipeline) => {
   pipeline.push({
     $project: {
       ticker: 1,
@@ -98,6 +98,45 @@ const filter = (options) => {
     }
   });
 
+};
+
+const prepare = (results) => {
+  return _
+    .chain(results)
+    .map((row) => {
+      return {
+        ticker: row.ticker,
+        params: _.pick(row, ['margin', 'PE', 'PB'])
+      }
+    })
+    .keyBy('ticker')
+    .value();
+};
+
+const accountPortfolio = (options) => {
+  // const DerivedTrailingTwelveMonthsEarningsPerShareDiluted
+  const paths = {
+    intrinsicValue: 'annual.DerivedDCF_IntrinsicValue_MAX_GROWTH_RATE_20_BY_MEAN',
+    earnings: 'quarterly.DerivedTrailingTwelveMonthsEarningsPerShareDiluted', //'annual.EarningsPerShareDiluted',
+    bookValue: 'annual.DerivedBookValuePerShare',
+  };
+
+  const aggregation = Filters.batch(
+    [
+      { path: paths.intrinsicValue },
+      { path: paths.earnings },
+      { path: paths.bookValue },
+      // { path: 'quarterly.FundamentalAccountingConcepts.ROE' },
+      // { path: 'quarterly.FundamentalAccountingConcepts.ROA' },
+    ],
+    options
+  );
+  // console.log(JSON.stringify(testAggregate, null, 2));
+
+  const { pipeline } = aggregation;
+
+  appendValuationsTo(paths, pipeline);
+
   pipeline.push({
     $match: {
       margin: { $ne : null }
@@ -114,11 +153,11 @@ module.exports = {
   filter: filter,
 };
 
-// const tickers = require('../../../lib/account/importers/degiro/resources/tickers.json');
-// filter({ tickers, date: new Date(2018, 4, 15) })
-//   .then(result => JSON.stringify(result, null, 2))
-//   .then(console.log)
-//   .catch(console.log)
+const tickers = require('../../../lib/account/importers/degiro/resources/tickers.json');
+filter({ tickers, date: new Date(2018, 4, 15) })
+  .then(result => JSON.stringify(result, null, 2))
+  .then(console.log)
+  .catch(console.log)
 
 // Stocks.filter(Filters.filterBy__DerivedDCF_IntrinsicValue({ ticker: 'AAPL', date: '2017-09-30' }))
 //   .then(result => JSON.stringify(result, null, 2))
