a578d8c4426a36a52fe77bafe4f60faa15185b7e
ansteh <stehle.andre@googlemail.com>
2018-06-18 23:24:20 +0200

    fix available earnings

--- server/api/stocks/controller.js
+++ server/api/stocks/controller.js
@@ -153,11 +153,11 @@ module.exports = {
   filter: filter,
 };
 
-const tickers = require('../../../lib/account/importers/degiro/resources/tickers.json');
-filter({ tickers, date: new Date(2018, 4, 15) })
-  .then(result => JSON.stringify(result, null, 2))
-  .then(console.log)
-  .catch(console.log)
+// const tickers = require('../../../lib/account/importers/degiro/resources/tickers.json');
+// filter({ tickers, date: new Date(2018, 4, 15) })
+//   .then(result => JSON.stringify(result, null, 2))
+//   .then(console.log)
+//   .catch(console.log)
 
 // Stocks.filter(Filters.filterBy__DerivedDCF_IntrinsicValue({ ticker: 'AAPL', date: '2017-09-30' }))
 //   .then(result => JSON.stringify(result, null, 2))
