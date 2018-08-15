b726a3dea100bc971f5e505ae7fefb943f54f847
ansteh <stehle.andre@googlemail.com>
2018-08-15 14:41:37 +0200

    update value of portfolio

--- server/api/portfolio/test.js
+++ server/api/portfolio/test.js
@@ -1,6 +1,8 @@
+const _         = require('lodash');
 const Portfolio = require('./controller.js');
 
 Portfolio.getBy({ date: new Date(2018, 6, 27) })
+  .then(stocks => _.filter(stocks, position => _.isEmpty(_.get(position, 'stock')) ))
   .then(result => JSON.stringify(result, null, 2))
   .then(console.log)
   .catch(console.log)
